fe4d5acd3c6049a8f573074128d3e66c3388f6a1
Junmo Park <91249513+momoJune@users.noreply.github.com>
2022-03-08 23:27:28 +0900

    sql 연습

---
+++ hr50.sql
@@ -0,0 +1,15 @@
+-- 1. 모든 사원의 이름(FIRST_NAME, LAST_NAME)을 조회하라.
+
+SELECT 
+	FIRST_NAME,LAST_NAME 
+FROM 
+	HR.EMPLOYEES;
+
+
+-- 2. 모든 사원의 모든 정보를 조회하라.
+SELECT 
+	*
+FROM 
+	HR.EMPLOYEES;
+
+-- 3. 모든 도시 명을 조회하라.
