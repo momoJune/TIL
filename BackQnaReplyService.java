c881e669badab7f7c874df5180cf3ef08cd0118b
Junmo Park <91249513+momoJune@users.noreply.github.com>
2022-03-22 23:49:13 +0900

    댓글기능

---
+++ BackQnaReplyService.java
@@ -0,0 +1,14 @@
+package kr.sga.gkmarket.qna.service;
+
+import kr.sga.gkmarket.qna.vo.BackQnaReplyVO;
+
+public interface BackQnaReplyService {
+	//댓글 추가
+	void insert(BackQnaReplyVO backQnaReplyVO);
+	//댓글 수정
+	void update(BackQnaReplyVO backQnaReplyVO);
+	//댓글 삭제
+	void delete(BackQnaReplyVO backQnaReplyVO);
+	//해당 게시물에 댓글이 몇개( 답변완료 확인용)
+	int commentCount(int ref);
+}
